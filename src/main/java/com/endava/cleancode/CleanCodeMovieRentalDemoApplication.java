51d5876b708415e6b331f9ef75463e356f53a931
GeorgeBacalu <georgebacalu83@gmail.com>
2023-06-23 12:44:23 +0300

    Initial commit

---
+++ src/main/java/com/endava/cleancode/CleanCodeMovieRentalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.endava.cleancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeMovieRentalDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanCodeMovieRentalDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/endava/cleancode/CleanCodeMovieRentalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.cleancode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCodeMovieRentalDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
